c29287b3186ab8765201e3ca77ed03637bccedae
Aryan07032005 <vallakatiaryan@gmail.com>
2025-01-03 10:02:47 +0530

    while loop

---
+++ Demo3.java
@@ -0,0 +1,10 @@
+public classDemo3{
+    public static void main(String[]args){
+        String username = "username";
+        String pwd = "user_001";
+        while(username == "user001") && (pwd == "user_001"){
+        System.out.println(x:"Hello User Happy New Year");
+        break;
+        }
+    }
+}
